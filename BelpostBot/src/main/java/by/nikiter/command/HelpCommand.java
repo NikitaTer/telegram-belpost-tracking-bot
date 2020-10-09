748a51f6e1fedeced6b5c545b95fa45289c8f73f
Nikita Tsiareshchanka <nikitatet100@gmail.com>
2020-10-09 13:33:05 +0300

    Added help command

---
+++ BelpostBot/src/main/java/by/nikiter/command/HelpCommand.java
@@ -0,0 +1,42 @@
+package by.nikiter.command;
+
+import by.nikiter.model.PropManager;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.ICommandRegistry;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class HelpCommand extends BotCommand {
+
+    private static final String IDENTIFIER = "help";
+    private static final String DESC = "Возвращает список всех команд";
+
+    private final ICommandRegistry commandRegistry;
+
+    public HelpCommand(ICommandRegistry commandRegistry) {
+        super(IDENTIFIER, DESC);
+        this.commandRegistry = commandRegistry;
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(PropManager.getMessage("help.start"));
+
+        commandRegistry.getRegisteredCommands().forEach(cmd -> sb
+                .append(cmd.toString()).append("\n\n"));
+
+        sb.append(PropManager.getMessage("help.end"));
+
+        try {
+            absSender.execute(new SendMessage(chat.getId(),sb.toString()).enableHtml(true));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
