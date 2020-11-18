f815ffcc4b2e52ec949d88fb0678658fbc11447f
Nikita Tsiareshchanka <nikitatet100@gmail.com>
2020-11-18 20:30:30 +0300

    Added new command that prints info about choosen tracking

---
+++ BelpostBot/src/main/java/by/nikiter/command/GetTrackingCommand.java
@@ -0,0 +1,74 @@
+package by.nikiter.command;
+
+import by.nikiter.model.PropManager;
+import by.nikiter.model.UserState;
+import by.nikiter.model.db.entity.UserTrackingEntity;
+import by.nikiter.model.db.service.ServiceManager;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetTrackingCommand extends BotCommand {
+
+    private static final String IDENTIFIER = "get_tracking";
+    private static final String DESC = "Выводит информацию о выбранном почтовом отправлении";
+
+    public GetTrackingCommand() {
+        super(IDENTIFIER, DESC);
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+        ServiceManager manager = new ServiceManager();
+        manager.openSession();
+
+        try {
+            SendMessage message;
+            if (manager.getUserService().hasTrackings(user.getUserName())) {
+                message = new SendMessage(chat.getId(),PropManager.getMessage("command.get_tracking.choose"))
+                        .setReplyMarkup(getKeyboard(manager.getUserService().getAllTrackings(user.getUserName())));
+                manager.getUserService().changeUserState(user.getUserName(), UserState.CHOOSING_TRACKING_TO_GET);
+            } else {
+                message = new SendMessage(chat.getId(),PropManager.getMessage("command.no_trackings"));
+                manager.getUserService().changeUserState(user.getUserName(), UserState.USING_BOT);
+            }
+            absSender.execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        manager.closeSession();
+    }
+
+    /**
+     * Method that makes inline keyboard out of user's trackings
+     * @param trackings list of user's trackings
+     * @return inline keyboard with user's trackings
+     */
+    private InlineKeyboardMarkup getKeyboard(List<UserTrackingEntity> trackings) {
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        int i=0;
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        for (UserTrackingEntity ute : trackings) {
+            if (i==2) {
+                rows.add(row);
+                row = new ArrayList<>();
+                i=0;
+            }
+            row.add(new InlineKeyboardButton(ute.getTrackingName()).setCallbackData(ute.getTracking().getNumber()));
+            i++;
+        }
+        rows.add(row);
+
+        return new InlineKeyboardMarkup(rows);
+    }
+}
